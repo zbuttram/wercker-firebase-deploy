d66ba0ed2a9ad85c2d17ed6842a4d3af9c1323dc
Zach Buttram <zach.buttram@gmail.com>
2015-12-25 03:08:12 -0800

    Name is an already used variable, fixed

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 npm install -g firebase-tools
-firebase deploy -f $WERCKER_WERCKER_FIREBASE_DEPLOY_NAME --token $WERCKER_WERCKER_FIREBASE_DEPLOY_TOKEN
+firebase deploy -f $WERCKER_WERCKER_FIREBASE_DEPLOY_FIREBASE --token $WERCKER_WERCKER_FIREBASE_DEPLOY_TOKEN
