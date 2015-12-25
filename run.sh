81f7ce6bfb163bf65f896414b4975afa8693e984
Zach Buttram <zach.buttram@gmail.com>
2015-12-25 02:55:52 -0800

    Update for using different firebases

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 npm install -g firebase-tools
-firebase deploy --token $WERCKER_WERCKER_FIREBASE_DEPLOY_URL
+firebase deploy -f $WERCKER_WERCKER_FIREBASE_DEPLOY_NAME --token $WERCKER_WERCKER_FIREBASE_DEPLOY_TOKEN
