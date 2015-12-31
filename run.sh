9f85cf80b751f24bcf88e1bce725df8093b2db12
Zach Buttram <zach.buttram@gmail.com>
2015-12-31 01:09:31 -0800

    Added non-interactive flag

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 npm install -g firebase-tools
-firebase deploy -f $WERCKER_WERCKER_FIREBASE_DEPLOY_FIREBASE --token $WERCKER_WERCKER_FIREBASE_DEPLOY_TOKEN
+firebase --non-interactive deploy -f $WERCKER_WERCKER_FIREBASE_DEPLOY_FIREBASE --token $WERCKER_WERCKER_FIREBASE_DEPLOY_TOKEN
